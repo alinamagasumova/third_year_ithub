489264c47b4d344669df0a8ffb064380701c62bb
Alina <malinka2002@mail.ru>
2022-11-22 13:39:55 +0300

    newnats

--- Go/bot/nats-ads.go
+++ Go/bot/nats-ads.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/nats-io/nats.go"
 )
+
+type NatsAd struct {
+	Msg string `json: "msg"`
+	Time int `json: "time"`
+}
 
 func initiateNats() {
 	nc, _ := nats.Connect("nats://95.165.107.100:4222")
 	nc.Subscribe("ith.bot.ads", func(msg *nats.Msg) {
-		// decode msg
-		// send msg to all chats in cache
-		// reply to nats msg with result
+	// decode msg
+	// send msg to all chats in cache
+	// reply to nats msg with result
+	msgbytes := msg.Data
+	var receivedMessage NatsAd
+	err := json.Unmarshal(msgbytes, &receivedMessage)
+	if err != nil {
+		fmt.Println(err)
+	}
 	})
-
+	// drop to gb
 }
