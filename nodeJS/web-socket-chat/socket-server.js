1c67dfd7e99ee185150f3d3fe88a0fc579cd7fea
Alina <malinka2002@mail.ru>
2022-12-10 20:07:53 +0300

    DONE

--- nodeJS/web-socket-chat/socket-server.js
+++ nodeJS/web-socket-chat/socket-server.js
@@ -13,22 +13,26 @@ io.sockets.on('connection', socket => {
 
     socket.emit('hello', users)
 
-    socket.on('user', usName => {
-        users[socket.id] = usName;
+    socket.on('user', userInfo => {
+        users[socket.id] = [userInfo.name, userInfo.color];
         console.log('added')
         socket.broadcast.emit('join', users)
     });
 
     socket.on('msg', msg => {
-        let message = [users[socket.id], msg]
+        let message = [users[socket.id][0], users[socket.id][1], msg]
         io.sockets.emit('message', message)
     })
 
     socket.on('giveInfo', ()=>socket.emit('getInfo', users))
 
+    socket.on('private', (msg)=>{
+        io.to(msg.recId).emit('privateMsg', {adName: users[socket.id][0], text: msg.text});
+    })
+
     socket.on('disconnect', ()=>{
-        io.sockets.emit('close', users[socket.id])
-        delete users[socket.id]
+        io.sockets.emit('close', {len: Object.keys(users).length, man: users[socket.id]})
+        delete users[socket.id];
         console.log('deleted')
     })
 })
